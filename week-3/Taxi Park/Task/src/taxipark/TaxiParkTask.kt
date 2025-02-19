f7d5ed632bd8804f1fe1b17a37bc6a333aba4521
Cristian Puenguenan <styven21121@gmail.com>
2025-02-19 17:42:24 -0500

    feat: taxi park completed

--- week-3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ week-3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -1,11 +1,14 @@
 package taxipark
 
+import kotlin.math.round
+
 /*
  * Task #1. Find all the drivers who performed no trips.
  */
 fun TaxiPark.findFakeDrivers(): Set<Driver> =
-    allDrivers.filter { trips.none { trip -> it.name == trip.driver.name } }.toSet()
-
+    allDrivers
+        .filter { trips.none { trip -> it.name == trip.driver.name } }
+        .toSet()
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
@@ -43,13 +46,13 @@ fun TaxiPark.findSmartPassengers(): Set<Passenger> =
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
- * Return any period if many are the most frequent, return `null` if there're no trips.
+ * Return any period if many are the most frequent, return `null` if there are no trips.
  */
 fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
     // Get the range of the numbers given the parameter
     val getRange: (Int) -> IntRange = {
         val start = (it / 10) * 10
-        start.. start + 9
+        start..start + 9
     }
 
     return trips
@@ -63,5 +66,20 @@ fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
  * Check whether 20% of the drivers contribute 80% of the income.
  */
 fun TaxiPark.checkParetoPrinciple(): Boolean {
-    TODO()
+    if (trips.isEmpty()) return false
+
+    val splitPercent = round((allDrivers.size * 0.20)).toInt()
+
+    val totalCost = trips.sumOf { it.cost }
+
+    val tripsByDriver = allDrivers
+        .map { driver -> driver to trips.filter { trip -> trip.driver.name == driver.name } }
+        .map { (driver, trips) -> driver to trips.sumOf { it.cost } }
+        .sortedByDescending { (_, cost) -> cost }
+
+    val twentyPercentCost = tripsByDriver.take(splitPercent).sumOf { it.second }
+
+    val eightyPercentCost = totalCost * 0.8
+
+    return (twentyPercentCost >= eightyPercentCost)
 }
